d4a0420388aeaf376b4aef21940374ae128e75f0
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-23 18:09:27 -0300

    test: Requisito 2 - criado teste para a função encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,22 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    word = "AABBCC"
+
+    assert encrypt_message(word, len(word)) == "CCBBAA"
+
+    assert encrypt_message(word, 0) == "CCBBAA"
+
+    assert encrypt_message(word, -1) == "CCBBAA"
+
+    assert encrypt_message(word, 3) == "BAA_CCB"
+
+    assert encrypt_message(word, 4) == "CC_BBAA"
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1, 1)
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(word, "1")
