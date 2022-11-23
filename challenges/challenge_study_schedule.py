ad9269b71563b41987f3730c29ee200e48ae844c
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-23 17:14:55 -0300

    feat: Requisito 1: função study_schedule implementada
    retorna a quantidade de estudantes ativos no horário alvo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,32 @@
+def validate_parameters(permanence_period, target_time):
+    if type(permanence_period) is not list or len(permanence_period) == 0:
+        return False
+    if type(target_time) is not int:
+        return False
+    return True
+
+
+def validate_permanence_period(permanence_period):
+    try:
+        for element1, element2 in permanence_period:
+            if type(element1) is not int or type(element2) is not int:
+                return False
+    except Exception:
+        return False
+    return True
+
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not validate_parameters(permanence_period, target_time):
+        return None
+
+    if not validate_permanence_period(permanence_period):
+        return None
+
+    count = 0
+
+    for entry, exit in permanence_period:
+        if entry <= target_time <= exit:
+            count += 1
+
+    return count
