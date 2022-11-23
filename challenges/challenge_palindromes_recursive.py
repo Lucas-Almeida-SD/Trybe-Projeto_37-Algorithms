fb1ac5ab278e51150593870429ae20fb9843e29a
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-23 18:20:46 -0300

    feat: Requisito 3 - função is_palindrome_recursive implementada
    verifica se uma palavra é palíndromo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,14 @@
+def is_palindrome_recursive_aux(word):
+    if len(word) == 1:
+        return word
+    else:
+        return word[-1] + is_palindrome_recursive_aux(word[:-1])
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(word, str) or not word:
+        return False
+
+    reversed_word = is_palindrome_recursive_aux(word)
+
+    return reversed_word == word
