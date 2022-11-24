f296412f6cc655baab9b8ca974120425ae5243d2
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-24 02:13:57 -0300

    feat: Requisito 6 - função is_palindrome_iterative implementada
    verifica se uma palavra é palíndromo de forma iterativa

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,11 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(word, str) or not word:
+        return False
+
+    last_index = len(word) - 1
+    reverted_word = ""
+
+    for index in range(len(word)):
+        reverted_word += word[last_index - index]
+
+    return reverted_word == word
