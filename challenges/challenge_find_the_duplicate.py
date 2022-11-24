294941ad89cb4ab290e5c126b16b178b40d1397b
lucas_almeida <lucas.almdiniz@gmail.com>
2022-11-24 02:04:31 -0300

    feat: Requisito 5 - função validate_nums implementada
    busca pelo elemento repetido na lista

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,47 @@
+def validate_nums(nums):
+    if not isinstance(nums, list):
+        return False
+
+    if len(nums) <= 1:
+        return False
+
+    for number in nums:
+        if not isinstance(number, int) or number < 0:
+            return False
+
+    return True
+
+
+def generate_numbers_dict(nums):
+    numbers_dict = {}
+
+    for number in nums:
+        if number in numbers_dict:
+            numbers_dict[number] += 1
+        else:
+            numbers_dict[number] = 1
+
+    return numbers_dict
+
+
+def generate_duplicate_numbers(numbers_dict):
+    duplicate_numbers = []
+
+    for key in numbers_dict:
+        if numbers_dict[key] >= 2:
+            duplicate_numbers.append(key)
+
+    return duplicate_numbers
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not validate_nums(nums):
+        return False
+
+    numbers_dict = generate_numbers_dict(nums)
+    duplicate_numbers = generate_duplicate_numbers(numbers_dict)
+
+    if len(duplicate_numbers) == 0 or len(duplicate_numbers) > 1:
+        return False
+
+    return duplicate_numbers[0]
